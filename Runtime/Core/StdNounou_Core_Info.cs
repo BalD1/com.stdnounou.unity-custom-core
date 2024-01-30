6e5aa29acc57a026c232b56d27a5a3823d60d7d3
Florian <florianhusson@hotmail.fr>
2024-01-30 17:22:36 +0100

    update ver

--- Runtime/Core/StdNounou_Core_Info.cs
+++ Runtime/Core/StdNounou_Core_Info.cs
@@ -1,9 +1,9 @@
-#define STDNOUNOU_CORE_1_3_2
+#define STDNOUNOU_CORE_1_4_2
 
 namespace StdNounou.Core
 {
 	public static class StdNounou_Core_Info
 	{
-		public const string VERSION = "1.3.2";
+		public const string VERSION = "1.4.2";
 	} 
 }
